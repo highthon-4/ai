d38d4312667d5747f28aed3ea203d12412388431
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-02-17 16:52:39 +0900

    init

---
+++ src/main/java/com/hightoon/HightoonApplication.java
@@ -0,0 +1,13 @@
+package com.hightoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HightoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HightoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hightoon/HightoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hightoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HightoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
